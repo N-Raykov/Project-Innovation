1eb47987bc177d3c71b1a13915446aee5d07c79b
Alexray <ajschmaal@gmail.com>
2024-03-04 18:30:50 +0100

    Experimental AI + New control balancing

--- Assets/SpeedBoostRing.cs
+++ Assets/SpeedBoostRing.cs
@@ -11,9 +11,13 @@ public class SpeedBoostRing : NetworkBehaviour{
         Debug.Log(other.name);
 
         PlayerNetwork player = other.transform.root.gameObject.GetComponent<PlayerNetwork>();
+        AI_Test ai_controller = other.transform.root.gameObject.GetComponent<AI_Test>();
         
         if (player != null) {
             player.AddSpeed(speedIncrease);
+        }
+        else if (ai_controller != null) {
+            ai_controller.AddSpeed(speedIncrease);
         }
 
     }
