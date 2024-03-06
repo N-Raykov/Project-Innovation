a173c86f0025202f3d2db8df08536a7f32c109f9
Dobrin-Cotea-Antonio <534902@student.saxion.nl>
2024-03-06 21:56:37 +0100

    1

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -5,10 +5,19 @@ public class LevelManager : MonoBehaviour{
 
     public LevelManager instance { get; private set; }
 
-    //[SerializeField] List<>
+    PlayerNetwork player;
+    int playerLap;
 
     private void Awake(){
-        
+        if (instance != null) {
+            Destroy(instance.gameObject);
+        }
+
+        instance = this;
+    }
+
+    private void Start(){
+        player=FindObjectOfType<PlayerNetwork>();
     }
 
 }
