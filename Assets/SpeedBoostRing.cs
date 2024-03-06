dd495f5644204146b630a20ae5b50f6e6fe23949
Alexray-source <ajschmaal@gmail.com>
2024-03-06 13:22:56 +0100

    Level iteration #2 DONE

--- Assets/SpeedBoostRing.cs
+++ Assets/SpeedBoostRing.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 
-public class SpeedBoostRing : NetworkBehaviour{
+public class SpeedBoostRing : MonoBehaviour{
 
     [SerializeField] float speedIncrease;
     PlayerNetwork lastPlayerHit;
